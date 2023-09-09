66868f4ecc07986da89cdb64e4cbd2a027473e11
ELNAO <luis28jair5@gmail.com>
2023-09-09 17:50:04 -0600

    Prueba subida

---
+++ src/src/app/countries/pages/by-capital-page/by-capital-page.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
+
+@Component({
+  selector: 'app-by-capital-page',
+  templateUrl: './by-capital-page.component.html',
+  styles: [
+  ]
+})
+export class ByCapitalPageComponent {
+
+  public countries:Country[] = [];
+
+  constructor(private countryService:CountriesService){
+
+  }
+
+  searchByCapital(term:string){
+    this.countryService.searchCapital(term)
+    .subscribe(countries =>{
+      this.countries = countries;
+    });
+  }
+
+
+}
